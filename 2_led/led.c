170f618aa67aa6b6b6886027e42935def0911b9a
Sassy2573 <chenyinlin0429@gmail.com>
2024-08-25 00:12:37 -0700

    1

--- 2_led/led.c
+++ 2_led/led.c
@@ -2,7 +2,7 @@
  * @Author: Sassy2573 chenyinlin0429@gmail.com
  * @Date: 2024-08-15 01:36:53
  * @LastEditors: Sassy2573 chenyinlin0429@gmail.com
- * @LastEditTime: 2024-08-24 23:56:51
+ * @LastEditTime: 2024-08-25 00:03:53
  * @FilePath: /code/Driver_Development/2_led/led.c
  * @Description: 
  */
---
+++ linux/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Wed Aug 14 06:20:58 PDT 2024"
+#define LINUX_COMPILE_BY "chen"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 4.9.4 (Linaro GCC 4.9-2017.01) "
